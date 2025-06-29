2e19e1782e3b5fcd804ae0f8150864dc237d44bc
Leonhardt2 <l-hollatz@web.de>
2025-06-29 13:10:33 +0200

    change import of mp4box with new lib file

---
+++ generated/lib/segmentValidation/segmentAnalyzer.js
@@ -0,0 +1,75 @@
+import * as MP4Box from './mp4box.all.js';
+
+(function (global, factory) {
+  typeof exports === "object" && typeof module !== "undefined"
+    ? factory(exports, require("global/window"), require("xmldom"))
+    : typeof define === "function" && define.amd
+    ? define(["exports", "global/window", "xmldom"], factory)
+    : ((global = global || self),
+      factory((global.segmentAnalyzer = {}), global.window, global.window));
+})(this, function (exports, window$2, xmldom) {
+
+    var analyzeSegment = async function analyzeSegment (init, segment, expectedDuration) {
+        
+        // set start of the segment behind the init
+        init.fileStart = 0;
+        segment.fileStart = init.byteLength;    
+        
+        var mp4boxfile = MP4Box.createFile();
+        var allSamples = [];
+        
+        var results = [];
+
+        let resultFinished = new Promise((resolve, reject) => {
+            mp4boxfile.onReady = function (info) {
+                for (const track of info.tracks) {
+                    mp4boxfile.setExtractionOptions(track.id, null, { nbSamples: Infinity });
+                    mp4boxfile.start();
+                }
+
+                mp4boxfile.flush();
+
+                setTimeout(() => {
+                    for (const track of info.tracks) {
+                        if (!allSamples[track.id]) continue;
+                        let samples = allSamples[track.id];
+
+                        // aggregate durations of the samples
+                        let durationUnits = samples.reduce((acc, cur) => acc + cur.duration, 0);
+                        let durationSeconds = durationUnits / track.timescale;
+
+                        console.log(`Track ${track.id}: Gesamtdauer = ${durationUnits} (${durationSeconds} Sekunden)`);
+                        console.log("Expected duration: " + expectedDuration + " - Real duration: " + durationSeconds);
+
+                        if (expectedDuration == durationSeconds) {
+                            console.log("Segment correct");
+                            results.push(true);
+                        } else {
+                            console.log("Segment duration incorrect!");
+                            results.push(false);
+                        }
+                        if (track.id == info.tracks.length) {
+                            resolve();
+                        }
+                    }
+                }, 1);
+            };
+        });
+        
+        // write samples to the respective track_id
+        mp4boxfile.onSamples = function (track_id, user, samples) {
+            if (!allSamples[track_id]) allSamples[track_id] = [];
+            allSamples[track_id].push(...samples);
+        };
+
+        mp4boxfile.appendBuffer(init);
+        mp4boxfile.appendBuffer(segment);
+
+        await resultFinished;
+        return results;
+    }
+
+    exports.analyzeSegment = analyzeSegment;
+
+    Object.defineProperty(exports, "__esModule", { value: true });
+});
